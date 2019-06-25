75ec4521f2f68ea9c47851cd53860231a97e71f5
Austin Giberti <austingibertigit config --global user.email austingiberti@gmail.com>
2019-06-24 19:43:28 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ class App extends React.Component {
 
   componentDidMount() {
     Promise.all([
-      fetch("https://blooming-forest-89993.herokuapp.com/"),
-      fetch("https://blooming-forest-89993.herokuapp.com/")
+      fetch("https://blooming-forest-89993.herokuapp.com/folders"),
+      fetch("https://blooming-forest-89993.herokuapp.com/notes")
     ])
       .then(([folderRes, noteRes]) => {
         if (!folderRes.ok) return folderRes.json().then(e => Promise.reject(e));
